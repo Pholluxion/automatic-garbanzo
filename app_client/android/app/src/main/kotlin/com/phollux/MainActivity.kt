2cfe112dde44f8123c4329592703204cbdbc7a71
Pholluxion <penalozadaniel00@gmail.com>
2024-01-04 16:25:38 -0500

    04/01/2024: create app

---
+++ app_client/android/app/src/main/kotlin/com/phollux/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.phollux.app_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_client/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:app_client/app/app.dart';
+import 'package:app_client/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
